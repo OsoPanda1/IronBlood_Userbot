2ed29269dfe381b486af30a816e15bfa123dfbe3
SdAnubis <120252809+OsoPanda1@users.noreply.github.com>
2024-11-09 05:45:04 -0600

    update database.py

--- utils/database.py
+++ utils/database.py
@@ -1,13 +1,33 @@
 import sqlite3
 
-def create_database():
-    conn = sqlite3.connect('database.db')
+def connect_db():
+    return sqlite3.connect('database.db')
+
+def check_ban(user_id):
+    conn = connect_db()
     cursor = conn.cursor()
-    
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS baneados (
-            user_id INTEGER PRIMARY KEY
-        )
-    ''')
-    
-   
+    cursor.execute("SELECT * FROM baneados WHERE user_id = ?", (user_id,))
+    result = cursor.fetchone()
+    conn.close()
+    return result is not None
+
+def ban_user(user_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute("INSERT OR IGNORE INTO baneados (user_id) VALUES (?)", (user_id,))
+    conn.commit()
+    conn.close()
+
+def unban_user(user_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM baneados WHERE user_id = ?", (user_id,))
+    conn.commit()
+    conn.close()
+
+def log_message(user_id, mensaje):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO mensajes (user_id, mensaje) VALUES (?, ?)", (user_id, mensaje))
+    conn.commit()
+    conn.close()
